5280b95b3fd8c11edeaf8c8debea6c4f76facc54
Hazenhower-IT <118206118+Hazenhower-IT@users.noreply.github.com>
2023-05-02 13:00:22 +0200

    test xr

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Cube from "../components/Cube.vue"
+import XrCube from "../components/XrCube.vue"
 
 
 const router = createRouter({
@@ -9,6 +10,18 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: Cube
+    },
+
+    {
+      path: '/cube',
+      name: 'cube',
+      component: Cube
+    },
+
+    {
+      path: '/xr-cube',
+      name: 'xr-cube',
+      component: XrCube
     },
     
   ]
