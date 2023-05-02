e7e4876468cd8015ffdea9053d011521782e8586
Hazenhower-IT <118206118+Hazenhower-IT@users.noreply.github.com>
2023-05-02 12:06:59 +0200

    test deploy

---
+++ src/firebase/index.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDW2eTiMoLcNp3DWKerjp5RDm5mHfaTmMY",
+  authDomain: "hazenhower-portfolio.firebaseapp.com",
+  projectId: "hazenhower-portfolio",
+  storageBucket: "hazenhower-portfolio.appspot.com",
+  messagingSenderId: "1077666196770",
+  appId: "1:1077666196770:web:1e1d93c749702a4464497b"
+};
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
